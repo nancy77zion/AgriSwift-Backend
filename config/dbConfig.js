7e389d0a2f5417309055dd265e6b2e4670922a57
nanc77zion <ebnancy710@gmail.com>
2024-04-27 02:15:41 +0100

    changed database to cloud database

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -10,10 +10,3 @@ module.exports = {
   DIALECT: process.env.DATABASE_DIALECT
 }
 
-// module.exports = {
-//   HOST: 'bze7upeat22hyfbt865b-mysql.services.clever-cloud.com',
-//   USER: 'uqrnbms7ukgvzvwe',
-//   PASSWORD: 'c1JqGh31ZFcQadlymYu1',
-//   DATABASE: 'bze7upeat22hyfbt865b',
-//   DIALECT: 'mysql'
-// }
