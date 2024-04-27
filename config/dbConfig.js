0396f0e07edeb5f53d02017633fa50ac9a5dd990
nanc77zion <ebnancy710@gmail.com>
2024-04-27 01:33:59 +0100

    first commit for my api endpoints

---
+++ config/dbConfig.js
@@ -0,0 +1,9 @@
+
+
+module.exports = {
+  HOST: 'bze7upeat22hyfbt865b-mysql.services.clever-cloud.com',
+  USER: 'uqrnbms7ukgvzvwe',
+  PASSWORD: 'c1JqGh31ZFcQadlymYu1',
+  DATABASE: 'bze7upeat22hyfbt865b',
+  DIALECT: 'mysql'
+}
