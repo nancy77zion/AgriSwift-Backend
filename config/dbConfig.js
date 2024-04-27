2712ad61a764d1ab71174002c251e0461d4937e9
nanc77zion <ebnancy710@gmail.com>
2024-04-27 02:14:20 +0100

    changed database to cloud database

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,9 +1,19 @@
+const dotenv = require('dotenv');
+dotenv.config();
 
 
 module.exports = {
-  HOST: 'bze7upeat22hyfbt865b-mysql.services.clever-cloud.com',
-  USER: 'uqrnbms7ukgvzvwe',
-  PASSWORD: 'c1JqGh31ZFcQadlymYu1',
-  DATABASE: 'bze7upeat22hyfbt865b',
-  DIALECT: 'mysql'
+  HOST: process.env.DATABASE_HOST,
+  USER: process.env.DATABASE_USER,
+  PASSWORD: process.env.DATABASE_PASSWORD,
+  DATABASE: process.env.DATABASE_NAME,
+  DIALECT: process.env.DATABASE_DIALECT
 }
+
+// module.exports = {
+//   HOST: 'bze7upeat22hyfbt865b-mysql.services.clever-cloud.com',
+//   USER: 'uqrnbms7ukgvzvwe',
+//   PASSWORD: 'c1JqGh31ZFcQadlymYu1',
+//   DATABASE: 'bze7upeat22hyfbt865b',
+//   DIALECT: 'mysql'
+// }
